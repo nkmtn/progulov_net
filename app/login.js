ef569abb6e6728a030c7e901322c522ee8a3f555
nkmtn <nkmtn10@gmail.com>
2020-05-08 13:34:27 +0300

    test login works now

--- app/login.js
+++ app/login.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : '',
-    database : 'nodelogin'
+    database : 'mydb'
 });
 
 var app = express();
@@ -28,7 +28,7 @@ app.post('/auth', function(request, response) {
     var username = request.body.username;
     var password = request.body.password;
     if (username && password) {
-        connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+        connection.query('SELECT * FROM user WHERE user_login = ? AND user_password_hash = ?', [username, password], function(error, results, fields) {
             if (results.length > 0) {
                 request.session.loggedin = true;
                 request.session.username = username;
